9c43e6c93b6e50daa84ddfddd65d217c71c3b658
Yao Liu <liuyao@gmail.com>
2026-06-15 23:20:39 +0800

    Hide interior solid angle samples

--- apps/3d-lattice-tiler/solver-worker.js
+++ apps/3d-lattice-tiler/solver-worker.js
@@ -1,4 +1,4 @@
-import { createTilingStream, tileSpecs } from "./engine.js?v=20260605-crystal-order";
+import { createTilingStream, tileSpecs } from "./engine.js?v=20260615-geometric-solid-angles";
 
 const SNAPSHOT_INTERVAL_MS = 80;
 
