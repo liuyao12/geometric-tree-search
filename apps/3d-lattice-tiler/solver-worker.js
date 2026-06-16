eaf7ea46c09925f3a30d6ce249118cfa70c880d2
Yao Liu <liuyao@gmail.com>
2026-06-16 09:50:56 +0800

    Inline candidate frontier stats

--- apps/3d-lattice-tiler/solver-worker.js
+++ apps/3d-lattice-tiler/solver-worker.js
@@ -1,4 +1,4 @@
-import { createTilingStream, tileSpecs } from "./engine.js?v=20260605-crystal-order";
+import { createTilingStream, tileSpecs } from "./engine.js?v=20260616-inline-frontier-stats";
 
 const SNAPSHOT_INTERVAL_MS = 80;
 
