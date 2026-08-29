150531138e1fa63bc89a582b7f027408747bc669
liuyao12 <liuyao@gmail.com>
2026-08-29 02:17:04 -0700

    Align A2 worker catalogue cache versions

--- apps/3d-lattice-tiler/growth-benchmark-worker.js
+++ apps/3d-lattice-tiler/growth-benchmark-worker.js
@@ -1,4 +1,4 @@
-import { createTilingStream, preprocessTilingSystem, tileSpecs } from "./engine.js?v=20260828-a2-sliced-v236";
+import { createTilingStream, preprocessTilingSystem, tileSpecs } from "./engine.js?v=20260829-a2-sliced8-v237";
 
 let activeSequence = 0;
 let stopToken = { stop: false, manual_pause: false, additional_time_ms: 0 };
--- apps/3d-lattice-tiler/solver-worker.js
+++ apps/3d-lattice-tiler/solver-worker.js
@@ -1,4 +1,4 @@
-import { createTilingStream, tileSpecs } from "./engine.js?v=20260828-a2-sliced-v236";
+import { createTilingStream, tileSpecs } from "./engine.js?v=20260829-a2-sliced8-v237";
 
 const MESSAGE_BATCH_INTERVAL_MS = 32;
 const MESSAGE_BATCH_LIMIT = 256;
