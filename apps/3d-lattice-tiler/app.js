53b48c5c276256947db021030eb1d722f5453bfc
liuyao12 <liuyao@gmail.com>
2026-08-31 10:58:09 -0700

    Bust the lattice catalogue dependency cache

--- apps/3d-lattice-tiler/app.js
+++ apps/3d-lattice-tiler/app.js
@@ -4,7 +4,7 @@ import {
   GCTS_CATALOG_MIN_PERIODIC_MOTIF_TILES,
   isGctsFigureVisibleInCatalog,
   tileSpecs
-} from "./engine.js?v=20260831-substitution-v272";
+} from "./engine.js?v=20260831-substitution-v273";
 
 const $ = (id) => document.getElementById(id);
 
