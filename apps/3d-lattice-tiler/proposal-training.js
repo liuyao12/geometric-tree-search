19b4b99cd5b41cc9e3735ddd83bd65c2a94a9853
liuyao12 <liuyao@gmail.com>
2026-08-30 20:24:30 -0700

    Refresh deployed A2 candidate data

--- apps/3d-lattice-tiler/proposal-training.js
+++ apps/3d-lattice-tiler/proposal-training.js
@@ -1,4 +1,4 @@
-import { createTilingStream, tileSpecs } from "./engine.js?v=20260830-a2-sliced9-v255";
+import { createTilingStream, tileSpecs } from "./engine.js?v=20260830-a2-sliced9-v256";
 import {
   createInitialProposalPopulation,
   growthCurveArea,
